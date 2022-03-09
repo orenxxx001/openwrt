e6b1412b7f4874399a1ab1e3e7b0b37f4ab1204a
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-09 11:42:22 +0800

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -182,7 +182,6 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.mode=ap
 			set wireless.default_radio${devidx}.ssid=OpenWrt
 			set wireless.default_radio${devidx}.encryption=none
-			set wireless.default_radio${devidx}.key=12345678
 EOF
 		uci -q commit wireless
 
